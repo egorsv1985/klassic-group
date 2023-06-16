af4992dc8ea0730136efd90d1903dc0a06579ee1
egorsv1985 <egorvs2009@gmail.com>
2023-06-16 12:13:09 +0300

    added breadcrumb

--- brickbel.by/public_html/bitrix/templates/brickbel/components/bitrix/breadcrumb/.default/template.php
+++ brickbel.by/public_html/bitrix/templates/brickbel/components/bitrix/breadcrumb/.default/template.php
@@ -11,28 +11,44 @@ global $APPLICATION;
 if (empty($arResult))
 	return "";
 
-
-$strReturn .= '<nav class="breadcrumb mb-5 mt-3 bx-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList" > ';
+$strReturn .= '
+<nav aria-label="breadcrumb">
+	<ol class="breadcrumb mb-5 mt-3 bx-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList"> ';
 
 $itemSize = count($arResult);
-for ($index = 0; $index < $itemSize; $index++) {
+
+// Check if there is a previous page
+if (isset($_SERVER['HTTP_REFERER'])) {
+	$previousPage = $_SERVER['HTTP_REFERER'];
+
+	$strReturn .= '
+	<li class="fs-14 breadcrumb-item bx-breadcrumb-item" id="bx_breadcrumb_0" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+        <a  href="' . $previousPage . '" title="Назад" itemprop="item">
+            Назад
+        </a>
+		<meta itemprop="position" content="' . ($index + 1) . '">
+		</li>
+    ';
+}
+
+for ($index = 1; $index < $itemSize; $index++) {
 	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-	$arrow = ($index > 0 ? '<i class="fa fa-angle-right"></i>' : '');
 
 	if ($arResult[$index]["LINK"] <> "" && $index != $itemSize - 1) {
 		$strReturn .= '
-			
-				<a class="fs-14 breadcrumb-item bx-breadcrumb-item" id="bx_breadcrumb_' . $index . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" href="' . $arResult[$index]["LINK"] . '" title="' . $title . '" itemprop="item">
-					' . $title . '
-				</a>
-				
-			';
+		<li class="fs-14 breadcrumb-item bx-breadcrumb-item" id="bx_breadcrumb_' . $index . '" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+            <a  href="' . $arResult[$index]["LINK"] . '" title="' . $title . '" itemprop="item">
+                ' . $title . '
+            </a>
+			<meta itemprop="position" content="' . ($index + 1) . '">
+			</li>
+        ';
 	} else {
 		$strReturn .= '
-			
-				
-				<span class="fs-14 breadcrumb-item active" aria-current="page">' . $title . '</span>
-			';
+		<li class="fs-14 breadcrumb-item active" id="bx_breadcrumb_' . $index . '" itemprop="itemListElement" aria-current="page" itemscope itemtype="http://schema.org/ListItem">
+            <span >' . $title . '</span>
+			<meta itemprop="position" content="' . ($index + 1) . '">
+        ';
 	}
 }
 
