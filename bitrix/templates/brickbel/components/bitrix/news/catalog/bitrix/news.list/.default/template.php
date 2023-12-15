3fa9fc96b464c554c456a6a4d2211ac373567a4a
egorsv1985 <egorvs2009@gmail.com>
2023-12-15 08:55:20 +0300

    fix

---
+++ bitrix/templates/brickbel/components/bitrix/news/catalog/bitrix/news.list/.default/template.php
@@ -0,0 +1,61 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+// print_r($arResult);
+?>
+
+
+<section class="products pb-5">
+	<div class="container">
+		<div class="row gx-4 gy-3 mb-4">
+
+			<? $counter= 1;
+				 foreach ($arResult["ITEMS"] as $arItem) :
+				if (CModule::IncludeModule("millcom.phpthumb")) {
+					$arItem["PREVIEW_PICTURE"]["WEBP"] = CMillcomPhpThumb::generateImg($arItem["PREVIEW_PICTURE"]["SRC"], 1);
+					$arItem["PREVIEW_PICTURE"]["PNG"] = CMillcomPhpThumb::generateImg($arItem["PREVIEW_PICTURE"]["SRC"], 2);
+					$arItem["PREVIEW_PICTURE"]["BOX"] = CMillcomPhpThumb::generateImg($arItem["PREVIEW_PICTURE"]["SRC"], 25);
+
+				}
+
+			?>
+				<?
+				$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+				$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+				?>
+				<div class="col-12 col-sm-6 col-md-3 pb-4">
+					
+						<div class="row">
+							<div class="col-12">
+								<div class="mb-4">
+									 <a href="<?= $arItem["PREVIEW_PICTURE"]["BOX"] ?>" class="fancy" data-fancybox="gallery-<?= $counter ?>">
+										<picture>
+											<source srcset="<?= $arItem["PREVIEW_PICTURE"]["WEBP"] ?>" type="image/webp"><img src="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" alt="<?= $arItem["PREVIEW_PICTURE"]["ALT"] ?>" title="<?= $arItem["PREVIEW_PICTURE"]["TITLE"] ?>" class="w-100 h-100 rounded">
+										</picture>
+									</a>
+								</div>
+								<span class="fs-20 fw-700 ff-roboto lh-11 d-block mb-3"><? echo $arItem["PROPERTIES"]["PRICE_2"]["VALUE"] ?>/м2</span>
+								<span class="fs-18 ff-roboto lh-11 d-block mb-4"><?= $arItem["NAME"] ?></span>
+								<a class="btn fs-20 fw-500 px-3 py-3 btn-primary bg-gradient w-100" href="<?= $arItem["DETAIL_PAGE_URL"] ?>" role="button" title="Подробнее">Подробнее</a>
+							</div>
+						</div>
+					
+				</div>
+			<? $counter++; ?>
+			<? endforeach; ?>
+		</div>
+		<? if ($arParams["DISPLAY_BOTTOM_PAGER"]) : ?>
+			<?= $arResult["NAV_STRING"] ?>
+		<? endif; ?>
+	</div>
+</section>
---
+++ o-produkte/zelenye-zabory/index.php
@@ -0,0 +1,301 @@
+<?
+define("PAGE", "FENCES");
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Зеленые заборы");
+
+?><section class="montage pb-5">
+	<div class="container">
+		<div class="row gy-4">
+			<div class="col-12 col-md-6 col-lg-4">
+				<div class="h-50 mb-3">
+					<div class="border rounded-2 p-4">
+						<div class="fs-20 ff-roboto fw-600 lh-12">
+							<h3>Сфера
+								использования
+							</h3>
+						</div>
+						<div class="fs-16 lh-12">
+							<p>
+								Садовые стены, спортивные поля, стороны и уголки
+								дороги, покрытие стен, выставочные стенды, крыши и вертикальные
+								ландшафтные
+								зоны
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="h-50">
+					<div class="border rounded-2 p-4">
+						<div class="fs-20 ff-roboto fw-600 lh-12">
+							<h3>Описание системы
+							</h3>
+						</div>
+						<div class="fs-16 lh-12">
+							<p>
+								Продукция Калифорнийское полотно обладает идеальным дизайном и естественным видом, это предоставляет Вам возможность быстро и
+								легко покрыть многие поверхности Вашего
+								окружения.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="d-none d-lg-block col-lg-4">
+				<div class="d-block">
+					<picture>
+						<source srcset="<?= SITE_TEMPLATE_PATH ?>/img/logo-fences.webp" type="image/webp"><img src="<?= SITE_TEMPLATE_PATH ?>/img/logo-fences.png" alt="Монтаж" class="w-100 d-block">
+					</picture>
+				</div>
+			</div>
+			<div class="col-12 col-md-6 col-lg-4">
+				<div class="h-50 mb-3">
+					<div class="border rounded-2 p-4">
+						<div class="fs-20 ff-roboto fw-600 lh-12">
+							<h3>Особенности
+							</h3>
+						</div>
+						<div class="fs-16 lh-12">
+							<p>
+								Монтаж забора Калифорнийское полотно является достаточно
+								простым. Его проволока оцинкована, покрыта ПВХ и устойчива к УФ. Калифорнийское полотно сохраняет свой естественный вид на
+								протяжении многих лет.
+							</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</section>
+<section>
+	<div class="container">
+
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">ТЕХНИЧЕСКИЕ ДАННЫЕ</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Размер</td>
+					<td class="py-4 text-center border border-primary">Высота: 100 см, 120 см, 150 см, 200 см</td>
+					<td class="py-4 text-center border border-primary">Размер рулона: 10 м</td>
+					<td class="py-4 text-center border border-primary">Диаметр рулона: 40 см</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Вес</td>
+					<td colspan="3" class="py-4 text-center">2,700 г/м2</td>
+				</tr>
+				<tr class="border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Оцинкованная проволока</td>
+					<td colspan="3" class="py-4 text-center">1,60 х 2 : 3,20 мм горячеоцинкованный двухкратнозакрученный 1,40 х 2 : 2,80 мм горячеоцинкованный двухкратнозакрученный</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Синтетическая трава</td>
+					<td colspan="3" class="py-4 text-center border border-primary">Уф устойчивая ПВХ трава</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Огнестойкость</td>
+					<td colspan="3" class="py-4 text-center border border-primary">B2 класс огнезащитная добавка</td>
+				</tr>
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Метод установки</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Рулон Калифорнийское полотно</td>
+					<td class="py-4 text-center border border-primary">Изготовленная в рулонах Калифорнийское полотно перед собой закрепляется с углов ограждения, фиксируется специальными соединительными проводами
+						интервалом в 50 см.</td>
+
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Панель Калифорнийское полотно</td>
+					<td class="py-4 text-center">Изготовленный в виде панели Калифорнийское полотно фиксируются
+						предварительно сформированным конструкционным соединительным
+						устройством.</td>
+				</tr>
+
+
+
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Цвета</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Искусственный газон</td>
+					<td class="py-4 text-center border border-primary">Зеленый</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Элемент соединения</td>
+					<td class="py-4 text-center">Зеленый</td>
+				</tr>
+
+
+
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Преимущество</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Внешний вид</td>
+					<td class="py-4 text-center border border-primary">Калифорнийское полотно обладает естественным видом и может быть эксплуатирован на протяжении многих лет без каких-либо затрат.</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Техническое обслуживание</td>
+					<td class="py-4 text-center">Продукция не нуждается в техническом обслуживании, чтобы держать ее в чистоте.</td>
+				</tr>
+				<tr class="border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Экологичный</td>
+					<td class="py-4 text-center">Не содержит вредных веществ, может быть использован Вами в саду или других местах.</td>
+				</tr>
+
+
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Упаковка Продукта</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Рулон Калифорнийское полотно</td>
+					<td class="py-4 text-center border border-primary">скреплены в рулоны шириной по 40 см и высотой в 100-200 см, упакованы вертикально чтобы не повредить продукцию</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Панель Калифорнийское полотно</td>
+					<td colspan="3" class="py-4 text-center">может загружаться в различных видах без повреждений панелей</td>
+				</tr>
+
+
+
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Условия Хранения</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Клей и производные</td>
+					<td class="py-4 text-center border border-primary">от 0 до +30 градусами</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Другие Материалы</td>
+					<td colspan="3" class="py-4 text-center">от 0 до +30 градусами</td>
+				</tr>
+
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Производство и Установка</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Рулон Калифорнийское полотно</td>
+					<td class="py-4 text-center border border-primary">Подготовка на производство: 3-5</td>
+					<td class="py-4 text-center border border-primary">Производство: 1-7 дней</td>
+					<td class="py-4 text-center border border-primary">Монтаж:1-3 дня</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Панель Калифорнийское полотно</td>
+					<td class="py-4 text-center border border-primary">Подготовка на производство: 3-5</td>
+					<td class="py-4 text-center border border-primary">Производство: 2-10
+						дней</td>
+					<td class="py-4 text-center border border-primary">Монтаж:1-3 дня</td>
+				</tr>
+
+
+
+
+			</tbody>
+		</table>
+		<table class="table table-striped table-hover border border-secondary rounded-3">
+			<thead class="border-0 table-secondary">
+				<tr>
+					<th colspan="4" class="text-center text-uppercase fw-700">Срок Гарантии</th>
+
+				</tr>
+			</thead>
+			<tbody class="fs-14">
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">УФ-гарантия</td>
+					<td class="py-4 text-center border border-primary">3 года</td>
+					<td class="py-4 text-center border border-primary">Допустимое отклонение 10%</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Огнестойкость</td>
+					<td class="py-4 text-center border border-primary">3 года</td>
+					<td class="py-4 text-center border border-primary">Допустимое отклонение 10%</td>
+				</tr>
+				<tr class=" border border-primary">
+					<td class="py-4 ps-4 fw-700 text-center border border-primary">Прочность волокна</td>
+					<td class="py-4 text-center border border-primary">3 года</td>
+					<td class="py-4 text-center border border-primary">Допустимое отклонение 10%</td>
+				</tr>
+
+
+			</tbody>
+		</table>
+
+	</div>
+</section>
+
+
+
+
+<? $APPLICATION->IncludeComponent(
+	"bitrix:catalog.section.list",
+	"fences",
+	array(
+		"ADDITIONAL_COUNT_ELEMENTS_FILTER" => "additionalCountFilter",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "7200",
+		"CACHE_TYPE" => "A",
+		"COMPONENT_TEMPLATE" => "fences",
+		"COUNT_ELEMENTS" => "Y",
+		"COUNT_ELEMENTS_FILTER" => "CNT_ACTIVE",
+		"FILTER_NAME" => "sectionsFilter",
+		"HIDE_SECTIONS_WITH_ZERO_COUNT_ELEMENTS" => "N",
+		"IBLOCK_ID" => "21",
+		"IBLOCK_TYPE" => "CONTENT",
+		"SECTION_CODE" => "",
+		"SECTION_FIELDS" => array(0 => "", 1 => "ID", 2 => "CODE", 3 => "XML_ID", 4 => "NAME", 5 => "SORT", 6 => "DESCRIPTION", 7 => "PICTURE", 8 => "DETAIL_PICTURE", 9 => "IBLOCK_TYPE_ID", 10 => "IBLOCK_ID", 11 => "IBLOCK_CODE", 12 => "IBLOCK_EXTERNAL_ID", 13 => "DATE_CREATE", 14 => "CREATED_BY", 15 => "TIMESTAMP_X", 16 => "MODIFIED_BY", 17 => "",),
+		"SECTION_ID" => $_REQUEST["SECTION_ID"],
+		"SECTION_URL" => "",
+		"SECTION_USER_FIELDS" => array(0 => "", 1 => "",),
+		"TOP_DEPTH" => "2"
+	)
+); ?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
